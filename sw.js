53816607a0744f97fccd68eea2fa7150b0bf6bb9
walter <waltervivar1133@gmail.com>
2020-07-10 17:22:41 -0500

    subiendo cambios de dynamic y medidas para iOS

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -55,13 +55,18 @@ const APP_SHELL_INMUTABLE = [
   });
 
   // borrar cache antigua
-  self.addEventListener('activate', e =>{
+  self.addEventListener('activate', e => {
 
       const respuesta = caches.keys().then(keys =>{
 
           keys.forEach(key =>{
 
             if( key !== STATIC_CACHE && key.includes( 'static')){
+
+              return caches.delete(key);
+            }
+
+            if( key !== DYNAMIC_CACHE && key.includes( 'dynamic')){
 
               return caches.delete(key);
             }
